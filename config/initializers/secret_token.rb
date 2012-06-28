f7d81b99a7dd55141da6a15d9a8046ab05d7ad0f
Luis Edymerchk <edyhack@gmail.com>
2012-06-27 22:25:15 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PyrographyArt::Application.config.secret_token = '4fba8b0b68cccd8de9f12abdca963b7892f50d8aaa5646dc67dae669dc7d284ff94acdfeb1950a62dca06232c291cd99e9a8ea152c9d8f94867b6c89d3f2c828'
